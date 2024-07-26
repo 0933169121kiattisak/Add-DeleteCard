5a03f6ab454c063cabe004515e9062eb0102ce7e
kiattisak phumeanam <kaitttsarokaittsaro@gmail.com>
2024-07-26 21:41:49 +0700

    add delete

---
+++ script.js
@@ -0,0 +1,77 @@
+// document.querySelector('.cards').addEventListener('click', e => {
+//     if(e.target.classList.contains('delete-btn')){
+//         if(!document.startViewTransition){
+//             e.target.parentElement.remove();
+//             return;
+//         }
+    
+//         e.target.parentElement.style.ViewTransitionName = 'targeted-card';
+//         document.startViewTransition(() => {
+//             e.target.parentElement.remove();
+//         });
+//     }
+    
+// })
+
+// document.querySelector('.add-btn').addEventListener('click', async(e) => {
+//     const template = document.getElementById('card');
+
+//     const $newCard = template.content.cloneNode(true);
+
+//     if(!document.startViewTransition){
+//         document.querySelector('cards').appendChild($newCard);
+//         return;
+//     }
+
+//     $newCard.firstELementChild.style.ViewTransitionName = 'targeted-card';
+
+//     $newCard.firstELementChild.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+
+//     const transition = document.startViewTransition(() => {
+//         document.querySelector('card').appendChild($newCard);
+//     });
+
+//     await transition.finished
+
+//     const rand = window.performance.now().toString().replace('.','_') + Math.floor(Math.random() * 1000);
+//     document.querySelector('.card .card:last-child').style.ViewTransitionName = `card-${rand}`
+// })
+
+
+document.querySelector('.cards').addEventListener('click', e => {
+	if (e.target.classList.contains('delete-btn')) {
+
+		if (!document.startViewTransition) {
+			e.target.parentElement.remove();
+			return;
+		}
+
+		e.target.parentElement.style.viewTransitionName = 'targeted-card';
+		document.startViewTransition(() => {
+			e.target.parentElement.remove();
+		});
+	}
+})
+
+document.querySelector('.add-btn').addEventListener('click', async (e) => {
+	const template = document.getElementById('card');
+
+	const $newCard = template.content.cloneNode(true);
+
+	if (!document.startViewTransition) {
+		document.querySelector('.cards').appendChild($newCard);
+		return;
+	}
+
+	$newCard.firstElementChild.style.viewTransitionName = 'targeted-card';
+	$newCard.firstElementChild.style.backgroundColor = `#${ Math.floor(Math.random()*16777215).toString(16)}`;
+	const transition = document.startViewTransition(() => {
+		document.querySelector('.cards').appendChild($newCard);
+	});
+
+	await transition.finished;
+
+	const rand = window.performance.now().toString().replace('.', '_') + Math.floor(Math.random() * 1000);
+	document.querySelector('.cards .card:last-child').style.viewTransitionName = `card-${rand}`;
+
+});
